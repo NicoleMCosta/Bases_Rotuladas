d283896149dbd299ad81a390b31dfd5cec6e5848
NicoleMCosta <nicolemartinsdacosta@gmail.com>
2025-10-30 11:15:33 -0300

    iniciando

---
+++ dataset.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void read_grapf(FILE *f){
+
+}
+
+void limiar(FILE *f){
+
+    //sugestão: limiar 1.2 para criar 3 componentes conexos
+}
+
+void clusters(FILE *f){
+
+    //cria file separando por tipos
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include "dataset.h"
+
+int main(){
+
+    return 0;
+}
