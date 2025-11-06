036eb6c26677a6002c0026b4e26236e81469d13a
NicoleMCosta <nicolemartinsdacosta@gmail.com>
2025-11-06 12:07:09 -0300

    main modificada

--- main.c
+++ main.c
@@ -56,14 +56,14 @@ for (float lim = limiar_inicial; lim > 0.0f; lim -= 0.001f) {
 
 
 int main() {
-    // int num_combinacoes = 0;
-    // int qtdfiles = 0;
+    int num_combinacoes = 0;
+    int qtdfiles = 0;
     
-    // FILE *csv1 = fopen("./bases/rotulada.csv", "r");
-    // if (csv1 == NULL) {
-    //     printf("Erro ao abrir base para leitura\n");
-    //     return 1;
-    // }
+    FILE *csv1 = fopen("./bases/rotulada.csv", "r");
+    if (csv1 == NULL) {
+        printf("Erro ao abrir base para leitura\n");
+        return 1;
+    }
 
 
     int num_combinacoes2 = 0;
@@ -75,7 +75,7 @@ int main() {
         return 1;
     }
 
-    // execute(csv1, num_combinacoes, &qtdfiles);
+    execute(csv1, num_combinacoes, &qtdfiles);
     execute(csv2, num_combinacoes2, &qtdfiles2);
     
     return 0;
