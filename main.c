cd7f01c890624c01a7e07616211a5385ec99b285
NicoleMCosta <nicolemartinsdacosta@gmail.com>
2025-11-06 12:11:46 -0300

    atualização na main

--- main.c
+++ main.c
@@ -2,55 +2,33 @@
 
 
 void execute(FILE *csv1, int num_combinacoes, int *qtdfiles){
-        char *dist_file = distancias(csv1, &num_combinacoes, qtdfiles);
-        if (dist_file == NULL) {
-            printf("Erro ao gerar arquivo de distâncias.\n");
-            free(dist_file);
-            return;
-        }
-
-    // float lim = 0.150;
-    // int count = 0;
-    // for(float i = lim; i < 0.0; i--){
-    //     printf("loop %i %lf\n", count++,lim);
-
-    //     char *arquivo_limiar = limiar(lim, num_combinacoes, dist_file);
-    //     if (arquivo_limiar == NULL) {
-    //         printf("Erro ao criar limiar file\n");
-    //         free(arquivo_limiar);
-    //         return;
-    //     }
-
-    //     int compt = componentes(arquivo_limiar);  
-    //     if(compt == 3){
-    //         printf("Limiar a ser usado: %lf", lim);
-    //         break;
-    //     }else{
-    //         lim= lim - 0.001;
-    //     }
-
-    // }
+    char *dist_file = distancias(csv1, &num_combinacoes, qtdfiles);
+    if (dist_file == NULL) {
+        printf("Erro ao gerar arquivo de distâncias.\n");
+        free(dist_file);
+        return;
+    }
 
     float limiar_inicial = 0.150;
-int count = 0;
+    int count = 0;
 
-for (float lim = limiar_inicial; lim > 0.0f; lim -= 0.001f) {
-    printf("loop %d (lim = %.3f)\n", count++, lim);
+    for (float lim = limiar_inicial; lim > 0.0f; lim -= 0.001f) {
+        printf("loop %d (lim = %.3f)\n", count++, lim);
 
-    char *arquivo_limiar = limiar(lim, num_combinacoes, dist_file);
-    if (arquivo_limiar == NULL) {
-        printf("Erro ao criar arquivo de limiar.\n");
-        break;
+        char *arquivo_limiar = limiar(lim, num_combinacoes, dist_file);
+        if (arquivo_limiar == NULL) {
+            printf("Erro ao criar arquivo de limiar.\n");
+            break;
+        }
+
+        int compt = componentes(arquivo_limiar);
+        free(arquivo_limiar);
+
+        if (compt == 3) {
+            printf("Limiar a ser usado: %.3f\n", lim);
+            break;
+        }
     }
-
-    int compt = componentes(arquivo_limiar);
-    free(arquivo_limiar);
-
-    if (compt == 3) {
-        printf("Limiar a ser usado: %.3f\n", lim);
-        break;
-    }
-}
 
 }
 
@@ -75,7 +53,10 @@ int main() {
         return 1;
     }
 
+    printf("LIMIAR BASE 1\n");
     execute(csv1, num_combinacoes, &qtdfiles);
+
+    printf("\n\n\nLIMIAR BASE 2\n\n");
     execute(csv2, num_combinacoes2, &qtdfiles2);
     
     return 0;
